41084705276c3560eab7b6591d4976e0f801878b
sgohlke <gohlke.stefan@yahoo.de>
2023-09-08 03:59:31 +0200

    Fix HTML

--- webserver.ts
+++ webserver.ts
@@ -81,8 +81,12 @@ function handleRequest(request: Request): Response {
    responseHeaders.set('content-type', 'text/html; charset=UTF-8')
    return new Response(
       `
-    <html>
+    <!DOCTYPE html>
+    <html lang="en">
     <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Spread some love into the world!">
     </head>
     <body>
     <script>
