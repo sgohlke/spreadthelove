fbd23805f26fa55a10a9bc3ba98d27f23ce15ada
sgohlke <gohlke.stefan@yahoo.de>
2024-02-05 22:39:19 +0100

    Update dependencies and unstable flag

--- deps.ts
+++ deps.ts
@@ -1,4 +1,6 @@
 export {
    returnDataResponse,
-} from 'https://raw.githubusercontent.com/sgohlke/deno-web/8.2.0/response/responseFunctions.ts'
-export { JSON_CONTENT_TYPE_HEADER } from 'https://raw.githubusercontent.com/sgohlke/deno-web/8.2.0/response/ResponseHeaders.ts'
+} from 'https://raw.githubusercontent.com/sgohlke/deno-web/8.2.1/response/responseFunctions.ts'
+export { 
+   JSON_CONTENT_TYPE_HEADER 
+} from 'https://raw.githubusercontent.com/sgohlke/deno-web/8.2.1/response/ResponseHeaders.ts'
