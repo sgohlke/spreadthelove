60bb4394725db29af5c91ad7f1714f1e496792eb
sgohlke <gohlke.stefan@yahoo.de>
2024-03-10 17:42:40 +0100

    Use JSR

--- deps.ts
+++ deps.ts
@@ -1,6 +1,4 @@
 export {
+   JSON_CONTENT_TYPE_HEADER,
    returnDataResponse,
-} from 'https://raw.githubusercontent.com/sgohlke/deno-web/8.2.1/response/responseFunctions.ts'
-export { 
-   JSON_CONTENT_TYPE_HEADER 
-} from 'https://raw.githubusercontent.com/sgohlke/deno-web/8.2.1/response/ResponseHeaders.ts'
+} from 'jsr:@sgohlke/deno-web@^8.2.3'
